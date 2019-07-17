db88c3923ad13d1efc3ba3ebba6a3f8c69cbcd47
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-07-17 20:19:36 +0100

    adds xml conversion to server.js

---
+++ xmlconverter.js
@@ -0,0 +1,5 @@
+var convert = require('xml-js');
+var xml = require('fs').readFileSync('./flighdata_A.xml', 'utf8');
+var options = {ignoreComment: true, alwaysChildren: true};
+var flights = convert.xml2json(xml, options); // or convert.xml2json(xml, options)
+console.log(flights);
